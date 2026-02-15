713619c0e8426ba613ee646bd5ad30a5c5a97d96
Onso37 <80353484+Onso37@users.noreply.github.com>
2026-02-15 13:35:28 +0100

    Node for loop bounds

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/loops/LoopBounds.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/loops/LoopBounds.java
@@ -2,11 +2,31 @@ package pt.up.fe.specs.fortran.ast.nodes.loops;
 
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.expr.Expr;
+import pt.up.fe.specs.fortran.ast.nodes.variable.DataRef;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public class LoopBounds extends FortranNode {
+
     public LoopBounds(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
+    }
+
+    public DataRef getVar() {
+        return getChild(DataRef.class);
+    }
+
+    public Expr getLower() {
+        return getChild(Expr.class, 0);
+    }
+
+    public Expr getUpper() {
+        return getChild(Expr.class, 1);
+    }
+
+    public Optional<Expr> getStep() {
+        return getChildTry(Expr.class, 2);
     }
 }
