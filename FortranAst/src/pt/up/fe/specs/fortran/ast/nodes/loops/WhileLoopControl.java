9be8ae1f073cdcf02a15113266cbb323a093c753
Onso37 <80353484+Onso37@users.noreply.github.com>
2026-02-20 17:39:53 +0100

    Create classes for specialized loop control nodes

---
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/loops/WhileLoopControl.java
@@ -0,0 +1,26 @@
+package pt.up.fe.specs.fortran.ast.nodes.loops;
+
+import org.suikasoft.jOptions.Interfaces.DataStore;
+import pt.up.fe.specs.fortran.ast.FortranKeyword;
+import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.expr.Expr;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class WhileLoopControl extends ALoopControl {
+
+    public WhileLoopControl(DataStore data, Collection<? extends FortranNode> children) {
+        super(data, children);
+    }
+
+    public Optional<Expr> getCond() {
+        return getChildTry(Expr.class);
+    }
+
+    @Override
+    public String getCode() {
+        return keyword(FortranKeyword.WHILE)
+                + getCond().map(expr -> " (" + expr.getCode() + ")").orElse("");
+    }
+}
