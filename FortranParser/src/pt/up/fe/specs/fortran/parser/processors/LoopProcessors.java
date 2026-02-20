c1635152d82c3aee18a159a743bdd22810186103
Onso37 <80353484+Onso37@users.noreply.github.com>
2026-02-20 18:05:48 +0100

    Delete old classes

--- FortranParser/src/pt/up/fe/specs/fortran/parser/processors/LoopProcessors.java
+++ FortranParser/src/pt/up/fe/specs/fortran/parser/processors/LoopProcessors.java
@@ -1,9 +1,6 @@
 package pt.up.fe.specs.fortran.parser.processors;
 
-import pt.up.fe.specs.fortran.ast.nodes.loops.LoopBounds;
-import pt.up.fe.specs.fortran.ast.nodes.loops.LoopControl;
 import pt.up.fe.specs.fortran.ast.nodes.loops.LoopRange;
-import pt.up.fe.specs.fortran.ast.nodes.loops.enums.DoKind;
 import pt.up.fe.specs.fortran.ast.nodes.expr.DataRef;
 import pt.up.fe.specs.fortran.parser.FortranJsonResult;
 
