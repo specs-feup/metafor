282ee3205e0cb032b16f7f8b2516d39b9078826e
Sergey Korepanov <skorepanov@walletteam.org>
2026-03-18 12:03:01 +0300

    fix source test file

--- FortranParser/resources-test/fortran/parser/arrays/array_implied_do.f90
+++ FortranParser/resources-test/fortran/parser/arrays/array_implied_do.f90
@@ -5,18 +5,25 @@ PROGRAM test_implied_do
     integer, dimension(9) :: arr6
     integer, dimension(15) :: arr7
 
+    ! Basic implied-do
     arr1 = [(i, i = 1, 5)]
 
+    ! With expression
     arr2 = [(i * 10, i = 1, 5)]
 
+    ! With step
     arr3 = [(i, i = 1, 9, 2)]
 
+    ! Mixed values and implied-do
     arr4 = [0, (i, i = 1, 8), 99]
 
+    ! With type-spec
     arr5 = [integer :: (i * 2, i = 1, 10)]
 
+    ! Nested implied-do
     arr6 = [((i + j, i = 1, 3), j = 1, 3)]
 
-    c = [(i, i * 10, i * 100, i = 1, 5)]
+    ! Multiple values per iteration
+    arr7 = [(i, i * 10, i * 100, i = 1, 5)]
 
 END PROGRAM test_implied_do
