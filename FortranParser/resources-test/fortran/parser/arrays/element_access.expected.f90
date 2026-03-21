f5bf2b60a25ef173b04a0559e74e5193d3612cf3
Onso37 <afonso.vo@gmail.com>
2026-03-21 16:22:26 +0000

    Add test for simple array accesses

---
+++ FortranParser/resources-test/fortran/parser/arrays/element_access.expected.f90
@@ -0,0 +1,9 @@
+PROGRAM array_demonstration
+
+    integer :: vec(5) = [10, 20, 30, 40, 50]
+    integer :: matrix(3,3)
+
+    PRINT *, "The second element is:", vec(2)
+    PRINT *, "Multi dimensional:", matrix(1, 2)
+
+END PROGRAM array_demonstration
