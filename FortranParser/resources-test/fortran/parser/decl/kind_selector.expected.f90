1570abf4ffce482050af675cacf7d5bd58b6bcbe
Process-ing <rbruno.p.o@gmail.com>
2026-04-17 00:22:42 +0100

    Fix kind selector tests

--- FortranParser/resources-test/fortran/parser/decl/kind_selector.expected.f90
+++ FortranParser/resources-test/fortran/parser/decl/kind_selector.expected.f90
@@ -1,5 +1,5 @@
 PROGRAM kind_selector
-    integer(8) i8
+    integer(8) :: i8
 
     PRINT *, i8
 END PROGRAM kind_selector
--- FortranParser/resources-test/fortran/parser/decl/kind_selector.f90
+++ FortranParser/resources-test/fortran/parser/decl/kind_selector.f90
@@ -1,5 +1,5 @@
 program kind_selector
-    integer(8) i8
+    integer(8) :: i8
 
     print *, i8
 end program kind_selector
