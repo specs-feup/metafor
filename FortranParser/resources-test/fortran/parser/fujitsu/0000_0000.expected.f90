fac475c4dffdb3a080a68c50e5c02d4a3663a150
Process-ing <rbruno.p.o@gmail.com>
2026-04-16 16:29:49 +0100

    Write expected test file

---
+++ FortranParser/resources-test/fortran/parser/fujitsu/0000_0000.expected.f90
@@ -0,0 +1,12 @@
+PROGRAM main
+    integer*8 i8, j /-20/, k /20/
+    integer kkk /0/
+    DO i8 = j, k
+        kkk = kkk + i8
+    END DO
+    IF (int(kkk)==0) THEN
+        PRINT *, "OK"
+    ELSE
+        PRINT *, "NG", int(kkk)
+    ENDIF
+END
