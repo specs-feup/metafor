554bf25fcbc3260e7ae01a5574057dd92a49db51
Process-ing <rbruno.p.o@gmail.com>
2026-04-16 16:51:55 +0100

    Add kind to integer type

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/type/IntegerType.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/type/IntegerType.java
@@ -1,21 +1,36 @@
 package pt.up.fe.specs.fortran.ast.nodes.type;
 
+import org.suikasoft.jOptions.Datakey.DataKey;
+import org.suikasoft.jOptions.Datakey.KeyFactory;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Represents Fortran integer types.
  */
 public class IntegerType extends IntrinsicType {
+    public static final DataKey<Optional<Integer>> KIND = KeyFactory.optional("kind");
 
     public IntegerType(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
     }
 
+    public Optional<Integer> getKind() {
+        return get(KIND);
+    }
+
     @Override
     public String getCode() {
-        return "integer";
+        var kindOpt = getKind();
+
+        var code = new StringBuilder();
+
+        code.append("integer");
+        kindOpt.ifPresent(kind -> code.append("(").append(kind).append(")"));
+
+        return code.toString();
     }
 }
