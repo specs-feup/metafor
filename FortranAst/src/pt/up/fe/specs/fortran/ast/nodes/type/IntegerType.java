c019d360ef1877cf5c64a4b45f7f22ffdb414ed3
Process-ing <rbruno.p.o@gmail.com>
2026-04-17 00:21:43 +0100

    Output kind information for integer type

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/type/IntegerType.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/type/IntegerType.java
@@ -1,9 +1,8 @@
 package pt.up.fe.specs.fortran.ast.nodes.type;
 
-import org.suikasoft.jOptions.Datakey.DataKey;
-import org.suikasoft.jOptions.Datakey.KeyFactory;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.decl.KindSelector;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -12,25 +11,16 @@ import java.util.Optional;
  * Represents Fortran integer types.
  */
 public class IntegerType extends IntrinsicType {
-    public static final DataKey<Optional<Integer>> KIND = KeyFactory.optional("kind");
-
     public IntegerType(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
     }
 
-    public Optional<Integer> getKind() {
-        return get(KIND);
+    public Optional<KindSelector> getKindSelector() {
+        return getChildOf(KindSelector.class);
     }
 
     @Override
     public String getCode() {
-        var kindOpt = getKind();
-
-        var code = new StringBuilder();
-
-        code.append("integer");
-        kindOpt.ifPresent(kind -> code.append("(").append(kind).append(")"));
-
-        return code.toString();
+        return "integer" + getKindSelector().map(FortranNode::getCode).orElse("");
     }
 }
