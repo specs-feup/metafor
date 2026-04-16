03bbf13e27ada9dd4f979b2427d5b35e6e93e62d
Process-ing <rbruno.p.o@gmail.com>
2026-04-16 16:52:09 +0100

    Fix expected code

--- FortranParser/resources-test/fortran/parser/fujitsu/0000_0000.expected.f90
+++ FortranParser/resources-test/fortran/parser/fujitsu/0000_0000.expected.f90
@@ -1,6 +1,6 @@
 PROGRAM main
-    integer(8) i8, j /-20/, k /20/
-    integer kkk /0/
+    integer(8) :: i8, j /-20/, k /20/
+    integer :: kkk /0/
     DO i8 = j, k
         kkk = kkk + i8
     END DO
