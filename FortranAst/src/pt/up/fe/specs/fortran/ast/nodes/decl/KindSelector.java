877f5be38e954e9f519a6d3edd049338d54945c1
Process-ing <rbruno.p.o@gmail.com>
2026-04-16 23:19:43 +0100

    Refactor kind selector node

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/decl/KindSelector.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/decl/KindSelector.java
@@ -6,16 +6,27 @@ import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
 
 import java.util.Collection;
-import java.util.Optional;
 
-public abstract class KindSelector extends FortranNode {
-    public static final DataKey<Optional<Integer>> KIND = KeyFactory.optional("kind");
+public class KindSelector extends FortranNode {
+    public static final DataKey<Integer> KIND = KeyFactory.integer("kind");
+    public static final DataKey<Boolean> LEGACY = KeyFactory.bool("legacy");
 
     public KindSelector(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
     }
 
-    public Optional<Integer> getKind() {
+    public Integer getKind() {
         return get(KIND);
+    }
+
+    public boolean isLegacy() {
+        return get(LEGACY);
+    }
+
+    @Override
+    public String getCode() {
+        return isLegacy()
+                ? "*" + getKind()
+                : "(" + getKind() + ")";
     }
 }
