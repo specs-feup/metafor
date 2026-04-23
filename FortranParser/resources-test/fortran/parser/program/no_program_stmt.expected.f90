9adcbd3f233468456a2bbf180f48ea578ced78df
Process-ing <rbruno.p.o@gmail.com>
2026-04-23 14:47:14 +0100

    Add support for program units with no program statements

--- FortranParser/resources-test/fortran/parser/program/no_program_stmt.expected.f90
+++ FortranParser/resources-test/fortran/parser/program/no_program_stmt.expected.f90
@@ -1,2 +1,2 @@
 PRINT *, "Hello World!"
-END PROGRAM
+END
--- FortranParser/src/pt/up/fe/specs/fortran/parser/processors/ProgramProcessors.java
+++ FortranParser/src/pt/up/fe/specs/fortran/parser/processors/ProgramProcessors.java
@@ -31,7 +31,7 @@ public class ProgramProcessors extends ANodeProcessor {
 
     public void mainProgram(MainProgram mainProgram) {
 
-        var name = attributes().getString(mainProgram, "source", FlangName.PROGRAM_STMT, FlangName.NAME);
+        var name = attributes().getOptionalString(mainProgram, "source", FlangName.PROGRAM_STMT, FlangName.NAME);
         // [specification-part]
         mainProgram.addChild(getChild(mainProgram, FlangName.SPECIFICATION_PART));
         // [execution-part]
@@ -41,7 +41,7 @@ public class ProgramProcessors extends ANodeProcessor {
             mainProgram.addChild(getChild(mainProgram, FlangName.INTERNAL_SUBPROGRAM_PART));
         }
 
-        mainProgram.setOptional(MainProgram.PROGRAM_NAME, name);
+        mainProgram.set(MainProgram.PROGRAM_NAME, name);
     }
 
     public void specification(Specification specification) {
