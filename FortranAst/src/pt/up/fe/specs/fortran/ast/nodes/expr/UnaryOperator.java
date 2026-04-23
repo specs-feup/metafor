932bfc73ee036ae13d6ce4fb121dab9a2319c97f
Process-ing <rbruno.p.o@gmail.com>
2026-04-23 15:09:43 +0100

    Implement unary operator

---
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/expr/UnaryOperator.java
@@ -0,0 +1,30 @@
+package pt.up.fe.specs.fortran.ast.nodes.expr;
+
+import org.suikasoft.jOptions.Datakey.DataKey;
+import org.suikasoft.jOptions.Datakey.KeyFactory;
+import org.suikasoft.jOptions.Interfaces.DataStore;
+import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.expr.enums.UnaryOperatorKind;
+
+import java.util.Collection;
+
+public class UnaryOperator extends Expr {
+    public final static DataKey<UnaryOperatorKind> OP = KeyFactory.enumeration("op", UnaryOperatorKind.class);
+
+    public UnaryOperator(DataStore data, Collection<? extends FortranNode> children) {
+        super(data, children);
+    }
+
+    public Expr getOperand() {
+        return getChild(Expr.class, 0);
+    }
+
+    public UnaryOperatorKind getOp() {
+        return get(OP);
+    }
+
+    @Override
+    public String getCode() {
+        return getOp().getString() + getOperand().getCode();
+    }
+}
