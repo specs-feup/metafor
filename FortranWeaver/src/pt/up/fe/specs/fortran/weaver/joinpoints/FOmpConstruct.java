1e7a791c63e78f22566af1d10fc8876b0c20ff1b
Onso37 <80353484+Onso37@users.noreply.github.com>
2026-04-23 22:48:15 +0100

    Add a way to change the directive of an omp construct

--- FortranWeaver/src/pt/up/fe/specs/fortran/weaver/joinpoints/FOmpConstruct.java
+++ FortranWeaver/src/pt/up/fe/specs/fortran/weaver/joinpoints/FOmpConstruct.java
@@ -3,6 +3,7 @@ package pt.up.fe.specs.fortran.weaver.joinpoints;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
 import pt.up.fe.specs.fortran.ast.nodes.omp.OmpConstruct;
 import pt.up.fe.specs.fortran.ast.nodes.omp.clause.OmpClause;
+import pt.up.fe.specs.fortran.ast.nodes.omp.enums.OmpDirectiveKind;
 import pt.up.fe.specs.fortran.weaver.FortranJoinpoints;
 import pt.up.fe.specs.fortran.weaver.abstracts.joinpoints.AExpr;
 import pt.up.fe.specs.fortran.weaver.abstracts.joinpoints.AOmpClause;
@@ -36,6 +37,11 @@ public class FOmpConstruct extends AOmpConstruct {
                 .toList();
 
         ompConstruct.setClauses(clauseList);
+    }
+
+    @Override
+    public void setDirectiveImpl(String directive) {
+        ompConstruct.set(OmpConstruct.KINDS, OmpDirectiveKind.getKinds(directive));
     }
 
     @Override
