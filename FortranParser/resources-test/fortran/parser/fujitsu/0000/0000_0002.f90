5587b9cbfbe748ded83ad50f801dba2e6c0c5a40
Process-ing <rbruno.p.o@gmail.com>
2026-04-24 10:18:30 +0100

    Add new Fujitsu test case

---
+++ FortranParser/resources-test/fortran/parser/fujitsu/0000/0000_0002.f90
@@ -0,0 +1,31 @@
+integer*1 :: a1, a2
+real*4 :: b1, b2
+logical :: ok
+
+ok = .true.
+
+b1 = 1.1
+a1 = 1
+a2 = b1
+b2 = a1
+
+IF ((a2 - b2) .ne. 0) THEN
+    ok = .false.
+    PRINT *, "TEST1-NG =>", a2 - b2
+END IF
+
+b1 = -1.1
+a1 = -1
+a2 = b1
+b2 = a1
+
+IF ((a2 - b2) .ne. 0) THEN
+    ok = .false.
+    PRINT *, "TEST2-NG =>", a2 - b2
+END IF
+
+IF (ok) THEN
+    PRINT *, "OK"
+END IF
+
+END
