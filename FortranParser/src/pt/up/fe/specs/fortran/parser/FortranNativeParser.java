c958694aef036b55c1f1a6c75f1eb67b28c94729
João Bispo <jbispo@fe.up.pt>
2026-05-01 15:52:23 +0100

    [FortraParser] Updates dumper version to v22.0.0 and uses stable release

--- FortranParser/src/pt/up/fe/specs/fortran/parser/FortranNativeParser.java
+++ FortranParser/src/pt/up/fe/specs/fortran/parser/FortranNativeParser.java
@@ -20,12 +20,12 @@ import java.util.function.Function;
 public class FortranNativeParser {
     public static final int FLANG_VERSION = 22;
 
-    private static final boolean USE_RELEASE = false;
+    private static final boolean USE_RELEASE = true;
 
     private static final boolean SAVE_JSON = false;
 
     private static final String BASE_URL = "https://github.com/specs-feup/flang-dumper/releases/download/";
-    private static final String CURRENT_VERSION = "v1.0.4";
+    private static final String CURRENT_VERSION = "v22.0.0";
 
     private static final WebResourceProvider LINUX_DUMPER_NIGHTLY =
             WebResourceProvider.newInstance(BASE_URL + "nightly/",
