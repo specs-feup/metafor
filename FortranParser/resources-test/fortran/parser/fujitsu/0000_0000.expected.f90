100a6e7d66306e7bc6b07bc074840b1402b0a928
João Bispo <jbispo@fe.up.pt>
2026-05-01 15:16:24 +0100

    Reverted expected name of program in test to lowercase, to test github action

--- FortranParser/resources-test/fortran/parser/fujitsu/0000_0000.expected.f90
+++ FortranParser/resources-test/fortran/parser/fujitsu/0000_0000.expected.f90
@@ -1,4 +1,4 @@
-PROGRAM MAIN
+PROGRAM main
     integer*8 :: i8, j /-20/, k /20/
     integer :: kkk /0/
     DO i8 = j, k
@@ -9,4 +9,4 @@ PROGRAM MAIN
     ELSE
         PRINT *, "NG", int(kkk)
     END IF
-END PROGRAM MAIN
+END PROGRAM main
