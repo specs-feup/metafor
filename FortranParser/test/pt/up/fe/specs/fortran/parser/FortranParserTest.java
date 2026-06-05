99abe1c799792c53ae4a8b3f42c9c9d9fd598246
Process-ing <rbruno.p.o@gmail.com>
2026-06-05 02:04:11 +0100

    Add one more Fujitsu test case

--- FortranParser/test/pt/up/fe/specs/fortran/parser/FortranParserTest.java
+++ FortranParser/test/pt/up/fe/specs/fortran/parser/FortranParserTest.java
@@ -640,4 +640,16 @@ public class FortranParserTest {
             testNative("fujitsu/0000/0000_0030.f90");
         }
     }
+
+    @Test
+    void testFujitsu0000_0033() {
+        testJson("fujitsu/0000/0000_0033.json");
+    }
+
+    @Test
+    void testFujitsu0000_0033Native() {
+        if (SpecsPlatforms.isLinux()) {
+            testNative("fujitsu/0000/0000_0033.f90");
+        }
+    }
 }
