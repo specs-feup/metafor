97d828b14f55d8b7be886e3e00200cfb23f7a9ff
Process-ing <rbruno.p.o@gmail.com>
2026-06-05 01:36:32 +0100

    Fix subscript triplet structure

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/expr/SubscriptTriplet.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/expr/SubscriptTriplet.java
@@ -1,5 +1,7 @@
 package pt.up.fe.specs.fortran.ast.nodes.expr;
 
+import org.suikasoft.jOptions.Datakey.DataKey;
+import org.suikasoft.jOptions.Datakey.KeyFactory;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
 
@@ -7,21 +9,29 @@ import java.util.Collection;
 import java.util.Optional;
 
 public class SubscriptTriplet extends SectionSubscript {
+    public static DataKey<Boolean> HAS_START = KeyFactory.bool("hasStart");
+    public static DataKey<Boolean> HAS_END = KeyFactory.bool("hasEnd");
+    public static DataKey<Boolean> HAS_STRIDE = KeyFactory.bool("hasStride");
+
     public SubscriptTriplet(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
     }
 
-    public Expr getStart() {
-        return getChild(Expr.class, 0);
+    public Optional<Expr> getStart() {
+        return get(HAS_START)
+                ? Optional.of(getChild(Expr.class, 0))
+                : Optional.empty();
     }
 
-    public Expr getEnd() {
-        return getChild(Expr.class, 1);
+    public Optional<Expr> getEnd() {
+        return get(HAS_END)
+                ? Optional.of(getChild(Expr.class, get(HAS_START) ? 1 : 0))
+                : Optional.empty();
     }
 
     public Optional<Expr> getStride() {
-        return getNumChildren() > 2
-                ? Optional.of(getChild(Expr.class, 2))
+        return get(HAS_STRIDE)
+                ? Optional.of(getChild(Expr.class, getNumChildren() - 1))
                 : Optional.empty();
     }
 }
