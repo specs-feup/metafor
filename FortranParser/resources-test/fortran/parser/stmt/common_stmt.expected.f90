266e0fca297ccd20c3b8d92ec19b8274a75e82f5
Process-ing <rbruno.p.o@gmail.com>
2026-06-25 13:19:11 +0100

    Fix common statement test

--- FortranParser/resources-test/fortran/parser/stmt/common_stmt.expected.f90
+++ FortranParser/resources-test/fortran/parser/stmt/common_stmt.expected.f90
@@ -1,7 +1,7 @@
-PROGRAM common_stmt
+PROGRAM COMMON_STMT
     REAL :: x
     COMMON /data_block/ x, my_array(5)
     INTEGER :: my_array
 
     x = 42.0
-END PROGRAM common_stmt
+END PROGRAM COMMON_STMT
