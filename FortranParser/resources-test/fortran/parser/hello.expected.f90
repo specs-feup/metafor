9d923a106b2248ebd55e9b0adc094a4cb16f1ce4
Process-ing <rbruno.p.o@gmail.com>
2026-07-04 00:21:28 +0100

    Update first test

--- FortranParser/resources-test/fortran/parser/hello.expected.f90
+++ FortranParser/resources-test/fortran/parser/hello.expected.f90
@@ -2,6 +2,6 @@ PROGRAM HELLO
     PRINT *, "Hello, World!"
     PRINT 100, "Hello, World!", 2
     100
-    /*<.getCode() not implemented for node class pt.up.fe.specs.fortran.ast.nodes.stmt.FormatStmt>*/
+    /*<.getStmtCode() not implemented for node class pt.up.fe.specs.fortran.ast.nodes.stmt.FormatStmt>*/
     20 PRINT "(A, F6.3)", "Value = ", 3
 END PROGRAM HELLO
