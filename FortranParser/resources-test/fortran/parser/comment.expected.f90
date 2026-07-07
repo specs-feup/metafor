579a8425bd9bc160d228a2a9dadf47fb038056fc
Process-ing <rbruno.p.o@gmail.com>
2026-07-07 23:05:41 +0100

    Update comment test

--- FortranParser/resources-test/fortran/parser/comment.expected.f90
+++ FortranParser/resources-test/fortran/parser/comment.expected.f90
@@ -1,6 +1,6 @@
 ! This is a \comment
-PROGRAM hello
+PROGRAM HELLO
     ! This is "another comment"
-    PRINT *, "Hello, World!"  ! This is a trailing comment
-END PROGRAM hello
+    PRINT *, "Hello, ""World!"""  ! This is a trailing comment
+END PROGRAM HELLO
 ! This is a final comment
