f9be456cb76d19fcf43bff01069a10295372e914
Process-ing <rbruno.p.o@gmail.com>
2026-07-09 00:26:08 +0100

    Fix weaver node association

--- FortranWeaver/src/pt/up/fe/specs/fortran/weaver/joinpoints/FDoConstruct.java
+++ FortranWeaver/src/pt/up/fe/specs/fortran/weaver/joinpoints/FDoConstruct.java
@@ -1,7 +1,7 @@
 package pt.up.fe.specs.fortran.weaver.joinpoints;
 
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
-import pt.up.fe.specs.fortran.ast.nodes.stmt.DoStmt;
+import pt.up.fe.specs.fortran.ast.nodes.stmt.loop.DoConstruct;
 import pt.up.fe.specs.fortran.weaver.FortranJoinpoints;
 import pt.up.fe.specs.fortran.weaver.abstracts.joinpoints.ADoStatement;
 import pt.up.fe.specs.fortran.weaver.abstracts.joinpoints.AExecution;
@@ -9,35 +9,35 @@ import pt.up.fe.specs.fortran.weaver.abstracts.joinpoints.ALoopControl;
 
 import java.util.Objects;
 
-public class FDoStatement extends ADoStatement {
+public class FDoConstruct extends ADoStatement {
 
-    private final DoStmt doStmt;
+    private final DoConstruct doConstruct;
 
-    public FDoStatement(DoStmt doStmt) {
-        super(new FExecutableStatement(doStmt));
-        this.doStmt = doStmt;
+    public FDoConstruct(DoConstruct doConstruct) {
+        super(new FExecutableStatement(doConstruct));
+        this.doConstruct = doConstruct;
     }
 
     @Override
     public AExecution getBodyImpl() {
-        return FortranJoinpoints.create(doStmt.getBody(), AExecution.class);
+        return FortranJoinpoints.create(doConstruct.getBody(), AExecution.class);
     }
 
     @Override
     public ALoopControl getControlImpl() {
-        return FortranJoinpoints.create(doStmt.getControl().get(), ALoopControl.class);
+        return FortranJoinpoints.create(doConstruct.getControl().get(), ALoopControl.class);
     }
 
     @Override
     public String getKindImpl() {
-        return doStmt.getKind().toString().toLowerCase();
+        return doConstruct.getKind().toString().toLowerCase();
     }
 
     @Override
     public ADoStatement copyScopeImpl() {
-        DoStmt copiedDoStmt = (DoStmt) doStmt.copy();
+        DoConstruct copiedDoStmt = (DoConstruct) doConstruct.copy();
         copiedDoStmt.getBody().removeChildren();
-        return new FDoStatement(copiedDoStmt);
+        return new FDoConstruct(copiedDoStmt);
     }
 
     @Override
@@ -50,6 +50,6 @@ public class FDoStatement extends ADoStatement {
 
     @Override
     public FortranNode getNode() {
-        return doStmt;
+        return doConstruct;
     }
 }
