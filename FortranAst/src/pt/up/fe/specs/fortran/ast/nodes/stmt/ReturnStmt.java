466dba7127c257be714ca346714b7b5ea75856eb
Process-ing <rbruno.p.o@gmail.com>
2026-07-21 00:15:12 +0100

    Refactor return statement node

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/stmt/ReturnStmt.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/stmt/ReturnStmt.java
@@ -1,8 +1,9 @@
 package pt.up.fe.specs.fortran.ast.nodes.stmt;
 
+import org.suikasoft.jOptions.Datakey.DataKey;
+import org.suikasoft.jOptions.Datakey.KeyFactory;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
-import pt.up.fe.specs.fortran.ast.nodes.expr.IntLiteral;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -10,18 +11,20 @@ import java.util.Optional;
 import static pt.up.fe.specs.fortran.ast.FortranKeyword.RETURN;
 
 public class ReturnStmt extends ExecutableStmt {
+    public static final DataKey<Optional<Integer>> TARGET = KeyFactory.optional("target");
+
     public ReturnStmt(DataStore data, Collection<? extends FortranNode> children) {
         super(data, children);
     }
 
-    public Optional<IntLiteral> getTarget() {
-        return getChildOf(IntLiteral.class);
+    public Optional<Integer> getTarget() {
+        return get(TARGET);
     }
 
     @Override
     public String getStmtCode() {
         var targetOpt = getTarget();
-        var targetSuffix = targetOpt.map(target -> " " + target.getCode()).orElse("");
+        var targetSuffix = targetOpt.map(target -> " " + target).orElse("");
 
         return keyword(RETURN) + targetSuffix;
     }
