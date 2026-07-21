1e86e8b512dec93b73325b34d5a9f08c9dd4f62a
Process-ing <rbruno.p.o@gmail.com>
2026-07-21 19:21:17 +0100

    Change expected code in legacy fortran

--- FortranParser/resources-test/fortran/parser/fujitsu/0001/0001_0000.expected.f
+++ FortranParser/resources-test/fortran/parser/fujitsu/0001/0001_0000.expected.f
@@ -1,18 +1,18 @@
-      INTEGER :: a(1000)
+      INTEGER a(1000)
 
-      a = 0
-      a = a + 1
+      a=0
+      a=a+1
 
-      DO 10 i = 1, 1000
-      IF (a(i) /= 1) THEN
-      WRITE(6, *) "NG"
-      WRITE(6, *) "ELEMENT NUMBER = A(", i, ")"
-      GO TO 20
-      END IF
+      DO 10 i=1,1000
+      IF(a(i) /= 1)THEN
+      WRITE(6, *)"NG"
+      WRITE(6, *)"ELEMENT NUMBER = A(",i,")"
+      GOTO 20
+      ENDIF
    10 CONTINUE
 
-      WRITE(6, *) "OK"
+      WRITE(6, *)"OK"
 
    20 CONTINUE
       STOP
-      END PROGRAM
+      END
--- FortranParser/resources-test/fortran/parser/fujitsu/0001/0001_0007.expected.f
+++ FortranParser/resources-test/fortran/parser/fujitsu/0001/0001_0007.expected.f
@@ -1,50 +1,50 @@
-      INTEGER :: a(1000), b(1000), c(1000), code
-      DATA a / 1000*0 /, b / 1000*0 /, c / 1000*0 /
+      INTEGER a(1000),b(1000),c(1000),code
+      DATA a/1000*0/,b/1000*0/,c/1000*0/
 C
-      code = 0
-      CALL sub1(a, b, c, 1000, code)
+      code=0
+      CALL sub1(a,b,c,1000,code)
 C
-      IF (code == 0) WRITE(6, *) "OK"
+      IF(code==0)WRITE(6,*)"OK"
 C
       STOP
-      END PROGRAM
+      END
 C
-      SUBROUTINE sub1(a, b, c, n, code)
-      INTEGER :: a(n), b(n), c(n), code
+      SUBROUTINE sub1(a,b,c,n,code)
+      INTEGER a(n),b(n),c(n),code
 C
-      a(1:n) = b(1:n) + 1
-      b(1:n) = b(1:n) + 1
-      c(1:n) = c(1:n) + 1
+      a(1:n)=b(1:n)+1
+      b(1:n)=b(1:n)+1
+      c(1:n)=c(1:n)+1
 C
-      DO 10 i = 1, 1000
-      IF (a(i) /= 1) THEN
-      WRITE(6, *) "NG"
-      WRITE(6, *) "ELEMENT NUMBER = A(", i, ")"
-      code = 1
-      GO TO 20
-      END IF
+      DO 10 i=1,1000
+      IF(a(i)/=1)THEN
+      WRITE(6,*)"NG"
+      WRITE(6,*)"ELEMENT NUMBER = A(",i,")"
+      code=1
+      GOTO 20
+      ENDIF
    10 CONTINUE
 C
    20 CONTINUE
-      DO 30 i = 1, 1000
-      IF (b(i) /= 1) THEN
-      WRITE(6, *) "NG"
-      WRITE(6, *) "ELEMENT NUMBER = B(", i, ")"
-      code = 1
-      GO TO 40
-      END IF
+      DO 30 i=1,1000
+      IF(b(i)/=1)THEN
+      WRITE(6,*)"NG"
+      WRITE(6,*)"ELEMENT NUMBER = B(",i,")"
+      code=1
+      GOTO 40
+      ENDIF
    30 CONTINUE
 C
    40 CONTINUE
-      DO 50 i = 1, 1000
-      IF (c(i) /= 1) THEN
-      WRITE(6, *) "NG"
-      WRITE(6, *) "ELEMENT NUMBER = C(", i, ")"
-      code = 1
-      GO TO 60
-      END IF
+      DO 50 i=1,1000
+      IF(c(i)/=1)THEN
+      WRITE(6,*)"NG"
+      WRITE(6,*)"ELEMENT NUMBER = C(",i,")"
+      code=1
+      GOTO 60
+      ENDIF
    50 CONTINUE
 C
    60 CONTINUE
       RETURN
-      END SUBROUTINE sub1
+      END
