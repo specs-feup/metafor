3f39eb85476dad4e74d7683d7e54d357d876f36e
Process-ing <rbruno.p.o@gmail.com>
2026-07-22 14:46:08 +0100

    Add complex type node

---
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/type/ComplexType.java
@@ -0,0 +1,18 @@
+package pt.up.fe.specs.fortran.ast.nodes.type;
+
+import org.suikasoft.jOptions.Interfaces.DataStore;
+import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.decl.KindSelector;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class ComplexType extends IntrinsicType {
+    public ComplexType(DataStore data, Collection<? extends FortranNode> children) {
+        super(data, children);
+    }
+
+    public Optional<KindSelector> getKindSelector() {
+        return getChildOf(KindSelector.class);
+    }
+}
