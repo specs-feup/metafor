30863b408c91f64c4ce67a9dc310a39ce3945709
Process-ing <rbruno.p.o@gmail.com>
2026-08-08 20:26:58 +0100

    Fix name fetching for end function and end subroutine statements

--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/program/subprogram/EndFunctionStmt.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/program/subprogram/EndFunctionStmt.java
@@ -3,6 +3,7 @@ package pt.up.fe.specs.fortran.ast.nodes.program.subprogram;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.FortranKeyword;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.specification.interfaces.InterfaceFunction;
 import pt.up.fe.specs.fortran.ast.nodes.stmt.Stmt;
 
 import java.util.Collection;
@@ -12,9 +13,23 @@ public class EndFunctionStmt extends Stmt {
         super(data, children);
     }
 
+    public String getFunctionName() {
+        var parent = getParent();
+
+        if (parent instanceof Function function) {
+            return function.getName();
+        }
+
+        if (parent instanceof InterfaceFunction interfaceFunction) {
+            return interfaceFunction.getName();
+        }
+
+        throw new RuntimeException("EndFunctionStmt must be inside a Function or InterfaceFunction, but found: " + parent.getClass().getSimpleName());
+    }
+
     @Override
     public String getStmtCode() {
-        var functionName = getAncestor(Function.class).getName();
+        var functionName = getFunctionName();
         return keyword(FortranKeyword.END) + " " + keyword(FortranKeyword.FUNCTION) + " " + functionName;
     }
 }
--- FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/program/subprogram/EndSubroutineStmt.java
+++ FortranAst/src/pt/up/fe/specs/fortran/ast/nodes/program/subprogram/EndSubroutineStmt.java
@@ -3,6 +3,7 @@ package pt.up.fe.specs.fortran.ast.nodes.program.subprogram;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import pt.up.fe.specs.fortran.ast.FortranKeyword;
 import pt.up.fe.specs.fortran.ast.nodes.FortranNode;
+import pt.up.fe.specs.fortran.ast.nodes.specification.interfaces.InterfaceSubroutine;
 import pt.up.fe.specs.fortran.ast.nodes.stmt.Stmt;
 
 import java.util.Collection;
@@ -12,6 +13,20 @@ public class EndSubroutineStmt extends Stmt {
         super(data, children);
     }
 
+    public String getSubroutineName() {
+        var parent = getParent();
+
+        if (parent instanceof Subroutine subroutine) {
+            return subroutine.getName();
+        }
+
+        if (parent instanceof InterfaceSubroutine interfaceSubroutine) {
+            return interfaceSubroutine.getName();
+        }
+
+        throw new RuntimeException("EndSubroutineStmt must be inside a Subroutine or InterfaceSubroutine, but found: " + parent.getClass().getSimpleName());
+    }
+
     @Override
     public String getStmtCode() {
         // TODO(Process-ing): Remove this special case
@@ -19,7 +34,7 @@ public class EndSubroutineStmt extends Stmt {
             return keyword(FortranKeyword.END);
         }
 
-        var subroutineName = getAncestor(Subroutine.class).getName();
+        var subroutineName = getSubroutineName();
         return keyword(FortranKeyword.END) + " " + keyword(FortranKeyword.SUBROUTINE) + " " + subroutineName;
     }
 }
